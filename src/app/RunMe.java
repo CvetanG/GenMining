2a3d82b94c0d6607edc7fb9d13f62c3da10900f4
Cvetan Georgiev <cgg@abv.bg>
2017-12-31 15:00:59 +0200

    GenMining

---
+++ src/app/RunMe.java
@@ -0,0 +1,82 @@
+package app;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import com.dropbox.core.DbxApiException;
+import com.dropbox.core.DbxAuthInfo;
+import com.dropbox.core.DbxException;
+import com.dropbox.core.v2.DbxClientV2;
+
+import app.controllers.DropboxController;
+import app.controllers.ExcelController;
+import app.controllers.WebSitesParser;
+import app.entities.GMRowEntry;
+import app.entities.Utils;
+
+public class RunMe {
+
+	public static void main(String[] args) throws IOException, DbxApiException, DbxException {
+		
+		System.out.println("Start Program Genesis-Mining");
+		long startTime = System.currentTimeMillis();
+		
+		String localPath = "GenesisMinig.xlsx";
+		
+		String dropboxPath = "/Finance/Genesis_Mining/" + localPath;
+		
+    	String argAuthFileOutput = "authFile.app";
+    	
+    	DropboxController myDropbox = new DropboxController();
+        
+        DbxAuthInfo authInfo = myDropbox.createAuth(argAuthFileOutput);
+        
+        DbxClientV2 client = myDropbox.createClient(authInfo);
+        
+        File localFile = myDropbox.getFile(client, localPath, dropboxPath);
+        
+		FileInputStream fsIP = new FileInputStream(localFile);
+				
+		//Access the workbook                  
+		Workbook wb = new XSSFWorkbook(fsIP);
+		
+		WebSitesParser myParser = new WebSitesParser();
+		
+		GMRowEntry rowEtry_01 = myParser.getMoneroInfo();
+		
+		System.out.println("Done Parsing Websites!!!");
+		
+//		int zeroRow = myEntries.size();
+		
+		ExcelController myPOI = new ExcelController();
+		
+		myPOI.writeInExcel(wb, rowEtry_01);
+		
+		System.out.println("Done Inserting Rows in Excel File!!!");
+		
+		//Close the InputStream  
+		fsIP.close();
+		
+		//Open FileOutputStream to write updates
+		FileOutputStream output_file =new FileOutputStream(localFile);  
+		
+		//write changes
+		wb.write(output_file);
+		
+		//close the stream
+		output_file.close();
+		
+		myDropbox.uploadFile(client, localFile, dropboxPath);
+        
+        long endTime   = System.currentTimeMillis();
+		System.err.println(Utils.duration(startTime, endTime));
+		System.out.println("All Done!!!");
+
+	}
+
+}
